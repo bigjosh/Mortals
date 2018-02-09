04c7135c027916e29f611402eeb8b15f71e7319d
Josh Levine <github@junk.josh.com>
2018-02-08 23:37:34 -0500

    Bring up to new API, classify health, add serial debug output
    Game works, only single team, no animations

--- Mortals.ino
+++ Mortals.ino
@@ -20,24 +20,26 @@
  *
  *  Technical Details:
  *  A long press on the tile changes the color of the tile for prototyping (switch state 1 or 2)
- *
+ *  Ready to attack tile shows spin pattern. 
+ *  On successful attack, shows full brightness on attacked face(s).
+ *  In idle, tiles breath their team color. At full health, breathing takes 2 seconds to range 255-128 brightness. At minimum health, 10hrtz breathing at lower brightness. 
+ *  On injury, face where were were hit shows full RED which decays over 500ms.   
  * 
  */
 
 
-#define ATTACK_VALUE 5            // Amount of health you loose when attacked.
-#define ATTACK_DURRATION_MS 100   // Time between when we see first new neighbor and when we stop attacking. 
-#define HEALTH_STEP_TIME_MS 1000  // Health decremented by 1 unit this often
+#define ATTACK_VALUE 5          // Amount of health units you loose when attacked.
+#define ATTACK_DURRATION_MS   100   // Time between when we see first new neighbor and when we stop attacking. 
+#define HEALTH_STEP_TIME_MS   1000  // Health decremented by 1 unit this often
 
-#define INJURED_DURRATION_MS 100  // How long we stay injured after we are attacked. Prevents multiple hits on the same attack cycle. 
+#define INJURED_DURRATION_MS  100   // How long we stay injured after we are attacked. Prevents multiple hits on the same attack cycle. 
 
-#define INITIAL_HEALTH      60
-#define MAX_HEALTH        90
+#define INITIAL_HEALTH      60     
+#define MAX_HEALTH        90  
 
 
 byte team = 0;
 
-word health;
   
 Timer healthTimer;  // Count down to next time we loose a unit of health
 
@@ -54,10 +56,78 @@ byte mode = DEAD;
 Timer modeTimeout;     // Started when we enter ATTACKING, when it expires we switch back to normal ALIVE. 
                        // Started when we are injuried to make sure we don't get injuied multipule times on the same attack
 
+#include "Serial.h"
+
+ServicePortSerial sp;
+
 void setup() {
-  blinkStateBegin();
+  sp.begin();
+  sp.println("Hello!");
 }
 
+
+class Health {
+
+  byte value;
+  byte maxValue; 
+  
+  public: 
+  
+    Health( byte m_maxValue ) {   // Really just gives us a way to ensure MAX_HEALTH fits into a byte
+      maxValue=m_maxValue;
+    }
+      
+    bool isAlive() {
+      return value>0;
+    }
+  
+    void set( byte x ) {
+    
+      value = x;  
+    }
+    
+    byte get(void) {
+      
+      return value;
+      
+    }
+  
+    // All this should be replaced with bounded::integer
+    
+    
+    void increase( byte x ) {
+              
+      if ( (x > maxValue) ||   ( value > ( maxValue-x ) ) ) {     // Mind your overflows! Order counts, people!
+      
+          value = maxValue;
+      
+      } else {
+      
+        value += x;
+      
+      }
+    }
+  
+  
+  
+    void decrease( byte x ) {
+    
+      if ( x > value  ) {     // Mind your overflows! Order counts, people!
+      
+        value = 0;
+      
+      } else {
+      
+        value -= x;
+      
+      }
+    }
+    
+  
+};
+
+Health health(MAX_HEALTH); 
+  
 void loop() {
   
   // Update our mode first  
@@ -65,51 +135,64 @@ void loop() {
   if(buttonDoubleClicked()) {
     // reset game piece   
     mode=ALIVE;
-    health=INITIAL_HEALTH;
+    health.set(INITIAL_HEALTH);
     healthTimer.set(HEALTH_STEP_TIME_MS);
   }
   
   if (healthTimer.isExpired()) {
-        
-    if (health>0) {
-      health--;
-      healthTimer.set(HEALTH_STEP_TIME_MS);  
-    }
     
+  health.decrease( 1 );
+  
+  if (health.isAlive()) {
+        
+      healthTimer.set(HEALTH_STEP_TIME_MS);  
+    
+    sp.print("health:");
+    sp.println(health.get());
+    
+    } else {  // if (!health.isAlive())
+      
     mode = DEAD;      
+    
+  }
   
   }
   
   if ( mode != DEAD ) {
       
-    if(isAlone()) {
+    if (isAlone()) {
       
-      mode = ENGUARDE;      // Being lonesome makes us ready to attack!
+    mode = ENGUARDE;      // Being lonesome always makes us ready to attack!    
+        sp.println("enguarde");
     
     } else {  // !isAlone()
       
       if (mode==ENGUARDE) {     // We were ornery, but saw someone so we begin our attack in earnest!
-        
+      
+        sp.println("attack");
         mode=ATTACKING;
         modeTimeout.set( ATTACK_DURRATION_MS );
+    
       }
         
     }
-    
-    
+        
     if (mode==ATTACKING || mode == INJURED ) {
       
       if (modeTimeout.isExpired()) {
         mode=ALIVE;
+        sp.println("alive");    
       }
+    
     } 
+  
   } // !DEAD
     
   FOREACH_FACE(f) {
+          
+    if(didValueOnFaceChange(f)) {
     
-    byte neighborState = getNeighborState(f);   
-      
-    if(neighborStateChanged(f)) {
+      byte neighborState = getLastValueReceivedOnFace(f);   
         
     if ( mode == ATTACKING ) {
       
@@ -117,9 +200,12 @@ void loop() {
     
       if ( neighborState == INJURED ) {     
         
-        // TODO: We should really keep a per-face attack timer to lock down the case where we attack the same tile twice in a since interaction. 
+      // TODO: We should really keep a per-face attack timer to lock down the case where we attack the same tile twice in a since interaction. 
         
-        health = min( health + ATTACK_VALUE , MAX_HEALTH );
+      health.increase(ATTACK_VALUE);
+          sp.print("flesh from ");
+      sp.println(f);
+      
                 
       }
       
@@ -127,23 +213,19 @@ void loop() {
         
       if ( neighborState == ATTACKING ) {
         
-        health = max( health - ATTACK_VALUE , 0 ) ;
+      health.decrease(ATTACK_VALUE);
                                         
-        mode = INJURED;
+      mode = INJURED;
         
-        modeTimeout.set( INJURED_DURRATION_MS );
+      modeTimeout.set( INJURED_DURRATION_MS );
+      
+          sp.print("flesh to ");
+          sp.println(f);
+      
       
       }
         
-    } else if (mode==INJURED) {
-        
-        if (modeTimeout.isExpired()) {
-          
-          mode = ALIVE;
-          
-        }
-        
-      }
+    }     
      } 
   }  
   
@@ -153,11 +235,11 @@ void loop() {
   switch (mode) {
     
     case DEAD:
-      setColor( dim( RED , 4 ) );
+      setColor( dim( RED , 30 ) );
       break;
       
     case ALIVE:
-      setColor( dim( GREEN , (health.getHealth() * 31 ) / MAX_HEALTH ) );   
+      setColor( dim( GREEN , ( health.get() * MAX_BRIGHTNESS ) / MAX_HEALTH ) );   
       break;
     
     case ENGUARDE:
@@ -174,7 +256,7 @@ void loop() {
     
   }
            
-  setState( mode );       // Tell everyone around how we are feeling
+  setValueSentOnAllFaces( mode );       // Tell everyone around how we are feeling
     
 }
 
